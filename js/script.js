fa10182b8b7982f8492ebe1e64c6364f8c9afc00
JadeTiger88 <jadetiger88@gmail.com>
2015-09-03 22:36:53 -0700

    Add song skip and song selection

--- js/script.js
+++ js/script.js
@@ -1,33 +1,82 @@
-var song ="media/Linkin Park - A Place For My Head.mp3"; 
-var audio = new Audio(song); 
+var songElement;
+var audio;
+
 
 $(document).ready(function() {
 
+	songElement = $('#playlist li:first-child'); 
+	setupAudioPlayer(songElement); 
+
 	$('#play').click(function() {
-		audio.play();
-		$('#play').hide(); 
-		$('#pause').show();
-		$(audio).bind('timeupdate', showProgress); 
+		audioPlay();
 	}); 
 
 	$('#stop').click(function() {
-		audio.pause();
-		audio.currentTime = 0; 
-		$('#play').show(); 
-		$('#pause').hide();
+		audioStop();
 	}); 
 
 	$('#pause').click(function() {
-		audio.pause();
-		$('#play').show(); 
-		$('#pause').hide();
+		audioPause();
 	}); 
+
+	$('#next').click(function() {
+		audioPause(); 
+
+		var next = $('#playlist li.active').next();
+		if(next.length == 0) {
+			next = $('#playlist li:first-child');
+		}
+		setupAudioPlayer(next);
+		audioPlay();
+	});
+
+	$('#prev').click(function() {
+		audioPause(); 
+
+		var prev = $('#playlist li.active').prev();
+		if(prev.length == 0) {
+			prev = $('#playlist li:last-child');
+		}
+		setupAudioPlayer(prev);
+		audioPlay();
+	});
 
 	$('#volumeSilder').change(function() {
 		audio.volume = parseFloat(this.value/100); 
 	})
 
+	$('#playlist li').click(function() {
+
+		// change the active class
+		$('#playlist li.active').removeClass('active');
+		$(this).addClass('active');
+
+		// Stop current song and play the new one 
+		audioStop();
+		setupAudioPlayer($(this));
+		audioPlay();
+	});
 });
+
+function audioPlay() {
+	audio.play();
+	$('#play').hide(); 
+	$('#pause').show();
+	$(audio).bind('timeupdate', showProgress); 
+}
+
+function audioPause() {
+	audio.pause();
+	$('#play').show(); 
+	$('#pause').hide();
+	$(audio).unbind('timeupdate', showProgress); 
+	showProgress();	
+}
+
+function audioStop() {
+	audio.currentTime = 0; 
+	audioPause();
+}
 
 function formatTime(seconds) {
 		var sec = parseInt(seconds % 60); 
@@ -47,12 +96,38 @@ function formatTime(seconds) {
 
 function showProgress() {
 
-		currentTime 	= formatTime(audio.currentTime); 
-		totalTime 		= formatTime(audio.duration); 
-		$('#time').html(currentTime+'/'+ totalTime);
+	var currentTime 	= formatTime(audio.currentTime); 
+	var totalTime 		= formatTime(audio.duration); 
+	$('#time').html(currentTime+'/'+ totalTime);
 
-		if (audio.currentTime > 0) {
-			value = Math.floor ((audio.currentTime/audio.duration) * 100);
-		}
-		$('#bar').css('width', value+"%"); 
+	var value = 0; 
+	if (audio.currentTime >= 0) {
+		value = Math.floor ((audio.currentTime/audio.duration) * 100);
+	}
+
+	$('#bar').css('width', value+"%"); 
+}
+
+function setupAudioPlayer(element) {
+
+	var songPrefix = "media/Linkin Park - "; 
+	var songPostfix = ".mp3";
+	var coverPrefix  = "img/covers/"; 
+
+    // Get items info from the html
+	var title =  element.text(); 
+	var cover =  element.attr('cover'); 
+	var artist =  element.attr('artist'); 
+
+
+	// Set items to be display on the audio player
+	$('#albumTitle').text(artist+ ": "); 
+	$('#songTitle').text(title); 
+	$('#albumCover img').attr('src', coverPrefix+cover);
+	$('#playlist li.active').removeClass('active');
+	element.addClass('active'); 
+
+	// Init audio
+	audio = new Audio(songPrefix+title+songPostfix); 
+	$(audio).bind('loadedmetadata', showProgress); 
 }
